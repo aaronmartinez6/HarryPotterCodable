9961d9a6c805fdd53acc9cd288bf2a9091243b22
Aaron Martinez <aaronmartinez6@hotmail.com>
2017-12-05 16:23:56 -0700

    Create views

---
+++ HarryPotterCodable/CharacterDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.swift
+//  HarryPotterCodable
+//
+//  Created by Aaron Martinez on 12/5/17.
+//  Copyright © 2017 Aaron Martinez. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ HarryPotterCodable/CharactersTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CharactersTableViewController.swift
+//  HarryPotterCodable
+//
+//  Created by Aaron Martinez on 12/5/17.
+//  Copyright © 2017 Aaron Martinez. All rights reserved.
+//
+
+import UIKit
+
+class CharactersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
